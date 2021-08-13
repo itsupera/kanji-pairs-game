87ef11432f5930694ce0bd3c96d1fc829d443955
Itsupera <itsupera@gmail.com>
2021-08-13 16:25:40 +0200

    Matching pairs using a dummy critereon

--- src/Main.elm
+++ src/Main.elm
@@ -4,8 +4,10 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (style)
+import Array exposing (Array)
 
 import JukugoData exposing (jukugos)
+import Html.Attributes exposing (selected)
 
 -- MAIN
 
@@ -18,67 +20,92 @@ main =
 
 -- MODEL
 
+type alias Kanji = String
 
-type alias Card = 
-  { kanji: String
-  , selected: Bool
-  }
-
-defaultCard : Card
-defaultCard =
-  { kanji = "水"
-  , selected = False
-  }
 
 type alias Model =
-  {
-  cards : List Card
+  { kanjis: Array Kanji
+  , selected: Maybe (Int, Kanji)
+  , matches: List (Kanji, Kanji)
   }
 
 
 init : Model
 init =
-  { cards = List.map (\kanji -> {kanji = kanji, selected = False}) initKanjis }
+  { kanjis = initKanjis, selected = Nothing, matches = [] }
 
-initKanjis : List String
+initKanjis : Array String
 initKanjis =
   List.take 8 jukugos
   |> List.concatMap (\x -> String.split "" x)
+  |> Array.fromList
+
 
 -- UPDATE
 
 
 type Msg
-  = Clicked Card
+  = Clicked Int
 
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Clicked _ ->
-      model
+    Clicked idx ->
+      case Array.get idx model.kanjis of
+        Nothing -> model
+        Just kanji -> 
+          case model.selected of
+            Nothing -> {model | selected = Just (idx, kanji)}
+            Just (selectedIdx, selectedKanji) ->
+              if selectedIdx == idx
+              then {model | selected = Nothing}  -- cancel selection
+              else (
+                if matchingPair model.kanjis selectedIdx idx
+                then {model | selected = Nothing, matches = (updateMatches model.matches (selectedKanji, kanji)) }
+                else model
+              )
+
+matchingPair : Array Kanji -> Int -> Int -> Bool
+matchingPair kanjis firstSel secondSel =
+    firstSel == secondSel - 1
 
 
+updateMatches matches newMatch =
+    matches ++ [newMatch]
 
 -- VIEW
 
 
 view : Model -> Html Msg
 view model =
+  div [] [viewGrid model, viewMatches model.matches]
+
+viewGrid : Model -> Html Msg
+viewGrid model =
   div
     [ style "display" "grid"
     , style "grid-template-columns" "1fr 1fr 1fr 1fr"
     , style "grid-template-rows" "1fr 1fr 1fr 1fr"
     , style "background-color" "gray"
     ]
-    (List.map renderCard model.cards)
+    (model.kanjis |> Array.toIndexedList |> List.map (renderCard model.selected))
 
-renderCard : Card -> Html Msg
-renderCard card =
+renderCard : Maybe (Int, Kanji) -> (Int, Kanji) -> Html Msg
+renderCard maybeSelected (idx, kanji) =
     button
-      [ onClick (Clicked card)
+      [ onClick (Clicked idx)
       , style "margin" "5px"
-      , style "background-color" "white"
+      , style "background-color" (bgcolor maybeSelected idx)
       , style "font-size" "10ex"
       ]
-      [ text card.kanji ]
+      [ text kanji ]
+bgcolor : Maybe (Int, Kanji) -> Int -> String
+bgcolor maybeSelected idx =
+    maybeSelected
+    |> Maybe.map (\(selectedIdx, _) -> if selectedIdx == idx then "red" else "white")
+    |> Maybe.withDefault "white"
+
+viewMatches : List (Kanji, Kanji) -> Html Msg
+viewMatches matches =
+    div [] [matches |> List.map Debug.toString |> String.concat |> text ]
